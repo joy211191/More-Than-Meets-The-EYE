a257f3396218c0bf052e3b87bf399b5121e44d84
Joy Goldar <joy211191@gmail.com>
2020-09-06 19:53:16 +0200

    Small Level
    Level with the concept made.

---
+++ Assets/Scripts/MirrorScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorScript : MonoBehaviour
+{
+    [SerializeField]
+    Transform playerObject;
+    [SerializeField]
+    Transform cameraTransform;
+    [SerializeField]
+    Camera secondaryCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update () {
+        Quaternion cameraRotation = cameraTransform.rotation;
+        secondaryCamera.transform.rotation = cameraRotation;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = playerObject.position + new Vector3(0, 20, 0);
+    }
+}
---
+++ Assets/Scripts/PhaseShift.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PhaseShift : MonoBehaviour
+{
+
+    [SerializeField]
+    RawImage rawImage;
+    bool resetColor;
+    float alphaValue=0;
+    [SerializeField]
+    GameObject subjectiveBlip;
+
+
+    private void Update () {
+        if (resetColor) {
+            alphaValue += Time.deltaTime / 2;
+            rawImage.color = new Color(1, 1, 1, alphaValue);
+            if (alphaValue >= 1) {
+                Destroy(subjectiveBlip);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter (Collider other) {
+        rawImage.color = new Color(1, 1, 1, 0);
+        alphaValue = 0;
+        resetColor = true;
+    }
+
+}
