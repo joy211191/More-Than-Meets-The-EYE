91a77e87ca1ab4addedbc4c81ccb3b20e9868e8b
Joy Goldar <joy211191@gmail.com>
2020-09-13 23:39:46 +0200

    Updated PP

--- Assets/Scripts/PhaseShift.cs
+++ Assets/Scripts/PhaseShift.cs
@@ -6,10 +6,6 @@ public class PhaseShift : MonoBehaviour
 {
 
     [SerializeField]
-    RawImage rawImage;
-    bool resetColor;
-    float alphaValue=0;
-    [SerializeField]
     GameObject subjectiveBlip;
     RealityShifter realityShifter;
 
@@ -18,24 +14,11 @@ public class PhaseShift : MonoBehaviour
         realityShifter = FindObjectOfType<RealityShifter>();
     }
 
-    private void Update () {
-        if (resetColor) {
-            if (alphaValue < realityShifter.animationCurve.Evaluate((float)realityShifter.blipsCollected)) {
-                alphaValue += Time.deltaTime / realityShifter.multiplier;
-                rawImage.color = new Color(1, 1, 1, alphaValue);
-            }
-            if (alphaValue >= realityShifter.animationCurve.Evaluate((float)realityShifter.blipsCollected)) {
-                Destroy(subjectiveBlip);
-                Destroy(gameObject);
-            }
-        }
-    }
 
     private void OnTriggerEnter (Collider other) {
-        rawImage.color = new Color(1, 1, 1, 0);
-        alphaValue = 0;
-        resetColor = true;
         realityShifter.UpdateBlips();
+        Destroy(subjectiveBlip);
+        Destroy(gameObject);
     }
 
 }
--- Assets/Scripts/RealityShifter.cs
+++ Assets/Scripts/RealityShifter.cs
@@ -7,11 +7,44 @@ public class RealityShifter : MonoBehaviour
 {
     public AnimationCurve animationCurve;
     public int blipsCollected;
-    public RawImage rawImage;
     public float multiplier;
+    [SerializeField]
+    RawImage rawImage;
+    [SerializeField]
+    bool resetColor;
+    float alphaValue = 0;
+    [SerializeField]
+    float maxTime;
+    float timer;
+
+    private void Awake () {
+        timer = maxTime;
+    }
 
     public void UpdateBlips () {
+        rawImage.color = new Color(1, 1, 1, 0);
+        alphaValue = 0;
         blipsCollected++;
-        rawImage.color = new Color(1, 1, 1, animationCurve.Evaluate((float)blipsCollected));
+        resetColor = true;
+        timer = maxTime;
+    }
+
+
+
+    private void Update () {
+        if (resetColor) {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+                resetColor = false;
+        }
+        if (timer<=0) {
+            if (alphaValue < animationCurve.Evaluate((float)blipsCollected)) {
+                alphaValue += Time.deltaTime / multiplier;
+                rawImage.color = new Color(1, 1, 1, alphaValue);
+            }
+            if (alphaValue >= animationCurve.Evaluate((float)blipsCollected)) {
+                resetColor = false;
+            }
+        }
     }
 }
